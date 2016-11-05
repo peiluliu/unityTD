609b2e37a3c87c03da89c47a07a05950f00577ac
fantasysukun <188454747@qq.com>
2016-11-05 15:08:26 -0700

    Testing Done

---
+++ Assets/scripts/EnemyMovement.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyMovement : MonoBehaviour {
+
+    public float speed = 10f;
+
+    private Transform target;
+    private int wavepointIndex = 0;
+
+	// Use this for initialization
+	void Start () {
+        target = waypoints.points[0];
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 dir = target.position - transform.position;
+        transform.Translate(dir.normalized* speed* Time.deltaTime, Space.World);
+
+        if(Vector3.Distance(transform.position, target.position) <= 1f){
+            GetNextWaypoint();
+        }
+    }
+
+    void GetNextWaypoint()
+    {
+        if(wavepointIndex >= waypoints.points.Length - 1)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        wavepointIndex++;
+        target = waypoints.points[wavepointIndex];
+    }
+}
