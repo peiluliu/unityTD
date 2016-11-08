005f3997fb3c41aa60fbcbd249ce1523e16ef750
fantasysukun <188454747@qq.com>
2016-11-07 16:45:36 -0800

    All basic zombies setup are done
    #1 All basic zombies setup are done
    #2 If import package is not working, try to replace with all the assests
    and project setting I uploaded.

---
+++ Assets/scripts/Menu.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour {
+
+    public bool PauseTheGame = false;
+    public Transform menu;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            PauseTheGame = !PauseTheGame;
+        }
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Resume();
+        }
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            Restart();
+        }
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            Exit();
+        }
+
+        if (PauseTheGame)
+        {
+            menu.gameObject.SetActive(true);
+            Time.timeScale = 0;
+
+        }
+        else if (!PauseTheGame)
+        {
+            menu.gameObject.SetActive(false);
+            Time.timeScale = 1;
+        }
+    }
+
+    public void Restart()
+    {
+        PauseTheGame = false;
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    public void Resume()
+    {
+        PauseTheGame = false;
+    }
+
+    public void Exit()
+    {
+        Application.Quit();
+    }
+}
